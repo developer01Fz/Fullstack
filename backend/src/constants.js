e7498de675ecd64d6bea6dc13372264578c59bfd
developer01Fz <developer01.fzeetechz@gmail.com>
2024-01-06 16:31:48 +0530

    setup full cokis [parser and cors

--- backend/src/constants.js
+++ backend/src/constants.js
@@ -2,3 +2,4 @@ export const DB_NAME = "youtube"
 // mongodb+srv://Nik:youtube@cluster0.eesvjkf.mongodb.net
 // PORT=8000
 // MONGODB_URI =mongodb+srv://Nik:youtube@cluster0.eesvjkf.mongodb.net
+// CORS_ORIGIN= *
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,19 @@
 // require('dotenv').config({path:'./env'})
 import  dotenv from "dotenv";
 import connectDB from './db/index.js'
-
+import {app} from './app.js'
+const port = process.env.PORT
 dotenv.config({
   path:'./env'
 })
 
 
 connectDB()
+.then(()=>{
+  app.listen(port || 8000, ()=>{
+    console.log("port connect")
+  })
+})
 
 
 //---------------start----------------------------------
